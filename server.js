3ffec77768ad844da413ad5d48f1b975987bf1e0
Wang Chao <trampzju@gmail.com>
2017-03-12 23:18:39 +0800

    server.js

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-'use strict';
+var fs = require("fs");
+var http = require("http");
+var url = require("url");
+var server = http.createServer((request, response) => {
+  if (request.method == "GET") {
+      var req = url.parse(request.url);
+      var time = req.query.substr(4);
+      //console.log(time);
+      //console.log(time.substr(12,2));
+      var result1 = {"hour": 0, "minute": 0, "second": 0};
+      result1.hour = Number(time.substr(11,2)) + 8;
+      result1.minute = Number(time.substr(14,2));
+      result1.second = Number(time.substr(17,2));
+      response.end(JSON.stringify(result1));
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
-var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-mongoose.Promise = global.Promise;
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+      var date = new Date(req.query.substr(4));
+      var result2 = {"unixtime": date.getTime()};
+      response.end(JSON.stringify(result2));
+  }
 });
+server.listen(80);
