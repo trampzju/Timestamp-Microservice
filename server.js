dd3e34db178a0fd52b701ecb522decaae405560d
Wang Chao <trampzju@gmail.com>
2017-03-13 10:36:55 +0800

    server.js

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ var url = require("url");
 var server = http.createServer((request, response) => {
   if (request.url!=="/favicon.ico") {
       	var req = url.parse(request.url);
+	if(req.path.substr(1).length === 0) {
+		respnes.end("{\"unix\": null, \"natural\": null}");
+		return;
+	}
 	//console.log(req);
 	var reg = /[^0-9]/;
 	if(reg.test(req.path.substr(1))){
       		var time = req.path.substr(1).replace(/%20/g, " ");
 		//console.log(time);
       		var date = new Date(time);
-      		var result2 = {"unix": date.getTime(), "natural": date.toDateString()};
-      		response.end(JSON.stringify(result2));
+		if(isNaN(date)) {
+			respnes.end("{\"unix\": null, \"natural\": null}");
+		}
+		else {
+      			var result2 = {"unix": date.getTime(), "natural": date.toDateString()};
+      			response.end(JSON.stringify(result2));
+		}
 	}
 	else {
 		var date = new Date();
